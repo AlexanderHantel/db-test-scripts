1582797a3b4d554d8e24dd24125e960c2dcfe3aa
Alexander Hantel <moizey@mail.ru>
2025-09-12 15:33:14 +0200

    simulate a failure

--- test_count.sh
+++ test_count.sh
@@ -3,7 +3,7 @@ set -e
 
 echo "Running test: count users"
 count=$(sqlite3 test.db "SELECT COUNT(*) FROM employees;")
-if [ "$count" -eq 4 ]; then
+if [ "$count" -eq 5 ]; then
   echo "OK: found 4 employees"
   exit 0
 else
