732fd660ad65b0f7f3e12da2662a939f162a3f7d
Alexander Hantel <moizey@mail.ru>
2025-09-12 16:11:43 +0200

    rework failure message of failed test

--- test_count.sh
+++ test_count.sh
@@ -3,9 +3,9 @@
 echo "Running test: count users"
 count=$(sqlite3 test.db "SELECT COUNT(*) FROM employees;")
 if [ "$count" -eq 5 ]; then
-  echo "OK: found 4 employees"
+  echo "OK: found 5 employees"
   exit 0
 else
-  echo "FAIL: expected 4 employees, got $count"
+  echo "FAIL: expected 5 employees, got $count"
   exit 1
 fi
